3500b367d045966d669514f1cb8a2bc0c8eb6d56
eapdob <eapdob@gmail.com>
2017-11-18 14:04:10 +0200

    mobile version

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function() {
-    console.log("Hello js!");
+    var __window = $(window);
+    var __mobileIcon = $(".header__mobileMenuIcon");
+
+    // window resizing
+    __window.on("resize", function() {
+        if (__window.width() < 992) {
+            $(".header__nav").appendTo($(".header__mobileMenu"));
+            $(".header__mobileMenu").removeClass("open");
+        } else {
+            $(".header__nav").appendTo($(".header__desktopMenu"));
+        }
+    });
+
+    __window.trigger("resize");
+
+    __mobileIcon.on("click", function() {
+        var el = $(this).data("toggle");
+        $(el).toggleClass("open");
+    });
 });
